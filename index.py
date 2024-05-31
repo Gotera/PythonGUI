c1e3cbdb2b83d87207dc88f3234541952509dc23
OM <orlandomedeiros.junior@gmail.com>
2024-05-31 14:59:19 -0300

    251

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 import sys
 from main_window import MainWindow
 from display import Display
+from info import Info
 from environment import WINDOW_ICON_PATH
 from PySide6.QtWidgets import QApplication, QLabel
 from PySide6.QtGui import QIcon
-
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
@@ -13,6 +13,9 @@ if __name__ == "__main__":
   icon = QIcon(str(WINDOW_ICON_PATH))
   window.setWindowIcon(icon)
   app.setWindowIcon(icon)
+  
+  info = Info('Testeee')
+  window.addWidgetToVLayout(info)
   
   display = Display()
   window.addWidgetToVLayout(display)
---
+++ info.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QLabel
+from PySide6.QtCore import Qt
+from environment import SMALL_FONT_SIZE
+
+class Info(QLabel):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    
+  def confiStyle(self):
+    self.setStyleSheet(f'font-size: {SMALL_FONT_SIZE}px;')
+    self.setAlignment(Qt.AlignmentFlag.AlignRight)
