1c33f7464ef1bda441e286605c9c4fa422c01fba
OM <orlandomedeiros.junior@gmail.com>
2024-05-31 15:43:50 -0300

    setupTheme()

--- index.py
+++ index.py
@@ -5,9 +5,11 @@ from info import Info
 from environment import WINDOW_ICON_PATH
 from PySide6.QtWidgets import QApplication, QLabel
 from PySide6.QtGui import QIcon
+from styles import setupTheme
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
+  setupTheme()
   window = MainWindow()
   
   icon = QIcon(str(WINDOW_ICON_PATH))
