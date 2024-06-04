02798014ba4f2f3d84542062996d69a47961d4a4
OM <orlandomedeiros.junior@gmail.com>
2024-06-04 00:02:48 -0300

    Buttons and Grid

---
+++ components/buttons.py
@@ -0,0 +1,34 @@
+from PySide6.QtWidgets import QPushButton, QGridLayout
+from environment import MEDIUM_FONT_SIZE
+from utils.verify_buttons import isNumOrDot, isEmpty
+
+
+class Button(QPushButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configStyle()
+
+    def configStyle(self):
+        self.setStyleSheet(f'font-size: {MEDIUM_FONT_SIZE}px;')
+
+
+class ButtonsGrid(QGridLayout):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._gridMask = [
+            ['C', '◀', '^', '/'],
+            ['7', '8', '9', '*'],
+            ['4', '5', '6', '-'],
+            ['1', '2', '3', '+'],
+            ['',  '0', '.', '='],
+        ]
+        self._makeGrid()
+
+    def _makeGrid(self):
+        for i, row in enumerate(self._gridMask):
+            for j, button_text in enumerate(row):
+                button = Button(button_text)
+                if not isNumOrDot(button_text) and not isEmpty(button_text):
+                  button.setProperty('cssClass', 'specialButton')     
+                self.addWidget(button, i, j)
--- index.py
+++ index.py
@@ -2,8 +2,9 @@ import sys
 from components.main_window import MainWindow
 from components.display import Display
 from components.info import Info
+from components.buttons import Button, ButtonsGrid
 from environment import WINDOW_ICON_PATH
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
 from styles.styles import setupTheme
 
@@ -21,6 +22,9 @@ if __name__ == "__main__":
   
   display = Display()
   window.addWidgetToVLayout(display)
+  
+  buttonsGrid = ButtonsGrid()
+  window.vLayout.addLayout(buttonsGrid)
   
   window.adjustFixedSize()
   window.show()
