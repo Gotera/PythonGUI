419cff0f4d7ae3e16f8fc9450531a16cdbc1f28c
OM <orlandomedeiros.junior@gmail.com>
2024-06-04 00:03:10 -0300

    Appling design to Info

--- components/info.py
+++ components/info.py
@@ -5,6 +5,7 @@ from environment import SMALL_FONT_SIZE
 class Info(QLabel):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
+    self.confiStyle()
     
   def confiStyle(self):
     self.setStyleSheet(f'font-size: {SMALL_FONT_SIZE}px;')
