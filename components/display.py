d8b2363d1f21580d12f93389b1afaa0a359163fa
OM <orlandomedeiros.junior@gmail.com>
2024-06-16 16:23:47 -0300

    Signals and its functions.

--- components/display.py
+++ components/display.py
@@ -2,18 +2,20 @@ from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QLineEdit
 from PySide6.QtCore import Qt, Signal
 from environment import BIG_FONT_SIZE, TEXT_MARGIN, MINIMUM_WIDTH
-from utils.verify_buttons import isEmpty
+from utils.verifications import isEmpty, isNumOrDot
 
 
 class Display(QLineEdit):
-  eqTriggered = Signal() 
-  delTriggered = Signal() 
-  clearTriggered = Signal() 
-  
+  eqTriggered = Signal()
+  delTriggered = Signal()
+  clearTriggered = Signal()
+  inputTriggered = Signal(str)
+  operatorTriggered = Signal(str)
+
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.configStyle()
-    
+
   def configStyle(self):
     margins = [TEXT_MARGIN for _ in range(4)]
     self.setStyleSheet(f'font-size: {BIG_FONT_SIZE}px;')
@@ -21,27 +23,40 @@ class Display(QLineEdit):
     self.setMinimumWidth(MINIMUM_WIDTH)
     self.setAlignment(Qt.AlignmentFlag.AlignRight)
     self.setTextMargins(*margins)
-    
+
   def keyPressEvent(self, e: QKeyEvent) -> None:
     text = e.text().strip()
     key = e.key()
     KEYS = Qt.Key
-    
-    isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
-    isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete]
-    isEsc = key == KEYS.Key_Escape
-    
+
+    isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return, KEYS.Key_Equal]
+    isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete, KEYS.Key_D]
+    isEsc = key in [KEYS.Key_Escape, KEYS.Key_C]
+    isOperator = key in [
+      KEYS.Key_Plus, KEYS.Key_Minus, KEYS.Key_Slash, KEYS.Key_P
+    ]
+
     if isEnter:
       self.eqTriggered.emit()
-      return super().keyPressEvent(e)
-    
+      return e.ignore()
+
     if isDelete:
       self.delTriggered.emit()
-      return super().keyPressEvent(e)
-    
+      return e.ignore()
+
     if isEsc:
       self.clearTriggered.emit()
-      return super().keyPressEvent(e)
-    
+      return e.ignore()
+
+    if isOperator:
+      self.clearTriggered.emit()
+      return e.ignore()
+
     if isEmpty(text):
+      if text.lower() == 'p':
+        text = '^'
+      return e.ignore()
+
+    if isNumOrDot(text):
+      self.inputTriggered.emit(text)
       return e.ignore()
