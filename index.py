21952cf0003d28a1eed6ae51c75b730a34b103ab
OM <orlandomedeiros.junior@gmail.com>
2024-06-16 16:24:42 -0300

    Deleting unused resource

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import sys
 from components.main_window import MainWindow
 from components.display import Display
 from components.info import Info
-from components.buttons import Button, ButtonsGrid
+from components.buttons import ButtonsGrid
 from environment import WINDOW_ICON_PATH
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QIcon
